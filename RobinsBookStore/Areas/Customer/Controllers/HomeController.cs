df427b317ed44fb3ed5b4280e40e5c93af242fda
rkaur3121 <115298724+rkaur3121@users.noreply.github.com>
2023-04-07 11:51:19 -0400

    created SD.cs class in RobinsBooks.Utility added Customer area in Areas folder Move HomeController.cs to Area/Customer Move Home folder and _ViewImports.cshtml and _viewStart.cshtml files to Areas/ Customer/Views and deleted this files from original side.

--- RobinsBookStore/Areas/Customer/Controllers/HomeController.cs
+++ RobinsBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -8,8 +8,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RobinsBookStore.Controllers
+namespace RobinsBookStore.Area.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
---
+++ RobinsBooks.Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobinsBooks.Utility
+{
+   public static class SD
+    {
+    }
+}
