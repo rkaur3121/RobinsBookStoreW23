40f92e16b66479742e99da552a53691af0ab7319
rkaur3121 <115298724+rkaur3121@users.noreply.github.com>
2023-04-07 12:30:47 -0400

    add-migration AddDefaultIdentityMigration in NuGet package console and update-database

---
+++ RobinsBooks.DataAccess/Migrations/20230407162426_AddDefaultIdentityMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RobinsBooks.DataAccess.Migrations
+{
+    public partial class AddDefaultIdentityMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
